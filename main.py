566774fa08e237b03496557ec2e86f60164710d1
Ivan Alexander <warrenfreind@gmail.com>
2023-07-14 11:42:50 -0700

    First commit of project, goals and expectations comment

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Goals for this lesson:
+    Use lots of classes, especially inheritance 
+    Practice git (command line)
+    More practice outsourcing functions and classes out into different files
+"""
+
